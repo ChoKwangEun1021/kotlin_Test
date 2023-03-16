4767e9f1323fd42294047a9697e78e9007d38cc4
Cho KwangEun <rhkddms1021@gmail.com>
2023-03-16 17:23:47 +0900

    [create] create kotlin_test

---
+++ app/src/main/java/com/example/test_01/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.test_01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidfirst.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
